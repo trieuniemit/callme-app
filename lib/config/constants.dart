dfc5b04fde0757ef57698b12d60e243505d037dd
NiemTT <trieuniemit@gmail.com>
2020-02-08 13:22:03 +0700

    update something

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
-    static const String API_URL = 'http://192.168.0.102:8000/api/';
-    static const String SOCKET_URL = 'ws://192.168.0.102:8000/ws';
+    static const String API_URL = 'https://callme-server.herokuapp.com/api/';
+    static const String SOCKET_URL = 'wss://callme-server.herokuapp.com/ws';
 
     static const String LOGIN = 'login'; //POST
     static const String GET_CONTACT = 'contact'; //GET
